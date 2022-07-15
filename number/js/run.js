a713d98ce28f1764576b2b1376ea8f0f473f7963
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-07-15 10:10:45 +0530

    Add files via upload

---
+++ number/js/run.js
@@ -0,0 +1,22 @@
+function number(a)
+{
+   if(a>0)
+   {
+      var a='positive';
+   
+   }
+   else if(a<0){
+      var a='negative';
+   } 
+   
+   else{
+      var a='non'
+   }
+   return a;
+
+}
+var a=8;
+
+
+var n=number(a);
+console.log (n);
